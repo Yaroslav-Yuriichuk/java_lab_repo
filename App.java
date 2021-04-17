0b44e96788efcf987092449edc68b2cb478885fd
SlavikYur <74894929+SlavikYur@users.noreply.github.com>
2021-04-17 23:29:36 +0300

    Used constructors with different numbers of parameters
    In order to check if all of constructor work correctly

--- App.java
+++ App.java
@@ -6,11 +6,9 @@ public class App {
 		ElectricLamp philipsEcoHome = new ElectricLamp("Philips", "China", 11, 	730,
 				"E27", "White", 270, 3000, 1150, 61, 136);
 
-		ElectricLamp osram = new ElectricLamp("OSRAM", "China", 8, 730,
-				"E14", "Matt", 200, 2900, 806, 45, 90);
+		ElectricLamp osram = new ElectricLamp();
 		
-		ElectricLamp euroLight = new ElectricLamp("EuroLight", "China", 12, 365,
-				"E27", "Matt", 220, 4200, 960, 60, 123);
+		ElectricLamp euroLight = new ElectricLamp("EuroLight", "China", 12, 365);
 		
 		ElectricLamp.printStaticPurposeOfLamp();
 		
