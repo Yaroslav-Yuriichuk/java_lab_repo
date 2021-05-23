b5dc8f8522f54cf41ea7c729698f89ef92c85f0b
SlavikYur <mr.sluvik822@gmail.com>
2021-05-23 11:32:56 +0300

    Adding files of shipping project (Lab10) with database support

--- src/main/java/models/ItemOrdered.java
+++ src/main/java/models/ItemOrdered.java
@@ -1,28 +1,38 @@
 package models;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 
+@Entity
+@Table(name = "ITEM_ORDERED_TBL")
 @Getter
 @Setter
 @EqualsAndHashCode
 @AllArgsConstructor
+@NoArgsConstructor
 public final class ItemOrdered {
+	@Id
+	@GeneratedValue
 	private Integer id;
-    private String name;
-    private int price;
-    private double weight;
-    private int itemCount;
-    private ItemType itemType;
 
-    @Override
-    public String toString() {
-        return String.format("%d x %s (%d x %d hrn.)%n",
-                this.getItemCount(),
-                this.getName(),
-                this.getItemCount(),
-                this.getPrice());
-    }
+	private String name;
+	private int price;
+	private double weight;
+	private int itemCount;
+	private ItemType itemType;
+
+	@Override
+	public String toString() {
+		return String.format("%d x %s (%d x %d hrn.)%n", this.getItemCount(), this.getName(), this.getItemCount(),
+				this.getPrice());
+	}
 }
--- src/main/java/ua/lviv/iot/SpringApp.java
+++ src/main/java/ua/lviv/iot/SpringApp.java
@@ -2,10 +2,14 @@ package ua.lviv.iot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan({ "controllers", "services", "models"})
+@EntityScan("models")
+@EnableJpaRepositories("dal")
 public class SpringApp {
 
 	public static void main(String[] args) {
